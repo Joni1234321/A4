fa2df0b71d0e84a9fa822c33fb2750e7109ea8f5
Jonas Vendelbo Hansen <Joni1234@hotmail.dk>
2020-11-01 15:59:15 +0100

    implemented faster neighbour calculation, and int[][] now stores alive, neighbours and counter. Also added: Simpledraw (for faster performance), timestamps and descriptive comments for Lukas and Niels

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -6,8 +6,14 @@ public class GameOfLife {
 
     int step = 0;
 
-    int size;                   // Size of grid
-    int[][] grid;               // Data of grid
+    // Size of grid
+    int size;
+
+    // Data of grid:
+    // 1st bit is alive
+    // 2nd to 5th bit is neighbours
+    // 6th to 32nd bit is counter
+    int[][] grid;
 
     final int SNAPSHOTS = 4;                    // Those are rookie numbers
     boolean[][][] snapshots;
@@ -23,47 +29,111 @@ public class GameOfLife {
         for (int y = 0; y < size; y++)
             for (int x = 0; x < size; x++)
                 grid[x][y] = r.nextBoolean() ? 1 : 0;
+
+        createNeighbourGrid();
+
     }
     public GameOfLife (int initialState[][]) {
         this.size   = initialState.length;
         snapshots   = new boolean[SNAPSHOTS][size][size];
 
         grid        = initialState;
+
+        createNeighbourGrid();
     }
 
+    /* TIL LUKAS OG NIELS
+    * OK Lukas og Niels, her forklarer jeg lige hvordan det fungere
+    * I opgaven er der beskrevet at man skal gemme information i en int array
+    * Problemet er bare at en int bruger 32 bits of data, men vi har kun brug for en bit til at fortælle om den er i live eller ej
+    * Derfor vil der være 31 bits man ikke bruger til noget som helst, hvilket jo er træls -_-
+    * Derfor valgte jeg bruge hver int til både at opbevare data der beskrev om den var i live, hvor mange naboer den har, og hvor mange gange den har været i live
+    *
+    * En int består af 32 bits, hvilket kan repræsentere således:
+    * 00000000 00000000 00000000 00000000
+    *
+    * Her er en list over hvad hver bit beskriver
+    * bit 32 - 6: En counter der tæller antallet af gange cellen har været i live       - har en værdi mellem 0 og 100.000.000 ca.  (27 bits)
+    * bit  5 - 2: Naboer, hvor mange naboer                                             - har en værdi mellem 0 og 8                (4  bits)
+    * bit      1: I live                                                                - har en værdi mellem 0 og 1                (1  bit)
+    *
+    *
+    * Eksempel: hvis en int ser således ud
+    * 00000000 00000000 00001100 11001101
+    * Så kan man aflæse at
+    * bit 32 - 6: 00000000 00000000 00001100 11 = [51]  // Cellen har været i live 51 gange
+    * bit  5 - 2: 0110                          = [ 6]  // Cellen har 6 naboer
+    * bit      1: 1                             = [ 1]  // Cellen er i live
+    *
+    *
+    * Implementering:
+    * For hver cyklus skal man:
+    * Finde ud af om den er i live
+    * Finde ud af hvor mange naboer den har
+    * Inkrementer den gamle celles counter
+    *
+    * For at finde ud af om den er i live:
+    * Her skal man isolere den første bit, hvilket man gør således
+    * alive = (cell & 0b1)
+    *
+    * For at finde ud af hvor mange naboer den har:
+    * Her skal man isolere bit 2, 3, 4 og 5, hvilket man gør således
+    * neighbours = (cell & 0b11110)
+    *
+    * For at finde den gamle counter:
+    * Her skal man isolere alle bits undtagen de første 5
+    * counter = (cell & ~0b11111)
+    *
+    * For at inkrementere:
+    * Her skal man inkrementere fra den 6. bit, hvilket kan gøres således
+    * if (alive == 1) counter += 0b100000
+    * Men det kan man også skrive som
+    * counter += 0b100000 * alive
+    * Eller
+    * counter += (alive << 5)
+    *
+    * FRA JONAS */
     public void nextState () {
         int[][] nextGrid = new int[size][size];
 
+        // Calculate alive
         for (int y = 0; y < size; y++){
             for (int x = 0; x < size; x++) {
-                int alive = (grid[x][y] & 0b1);
-                int neighbours = liveNeighbours(x, y);
+                int alive       = (grid[x][y] &  0b1);                       // Alive      is bit 1
+                int neighbours  = (grid[x][y] &  0b11110) >> 1;              // Neighbours is bit 5-2
 
-                // BRAAANCHLESSS!S!!!!
-                nextGrid[x][y] = (grid[x][y] & ~0b1) + (alive << 1);                 // Increment next value, and clear 1st bit
-                nextGrid[x][y] |= ((neighbours | alive) - 0b11 == 0) ? 0b1 : 0b0;    // Set 1st bit
-
-                // if ((neightbours | alive) == 3) så er cellen i live
-                // derfor kan man - med 3 på begge sider
-                // (neightbours | alive) - 3 == 0
-
-
+                // Increment next value, and clear alive and neighbour bits
+                nextGrid[x][y]  = (grid[x][y] & ~0b11111) + (alive << 5);
+                // Set 1st bit as either alive or dead
+                nextGrid[x][y] |= ((neighbours | alive) == 0b11) ? 0b1 : 0b0;
             }
         }
+
+        // Calculate neighbours
+        for (int y = 0; y < size; y++){
+            for (int x = 0; x < size; x++) {
+                // if bit 1 is set, then it is alive, and therefore add neighbours to next grid
+                if ((nextGrid[x][y] & 0b1) == 0b1) addToNeighbours(nextGrid, x, y);
+            }
+        }
+
         grid = nextGrid;
         step++;
         checkPeriodic();
     }
 
-    int liveNeighbours (int x, int y) {
-        int n = 0;
+    void createNeighbourGrid () {
+        for (int y = 0; y < size; y++)
+            for (int x = 0; x < size; x++)
+                if ((grid[x][y] & 0b1) == 1) addToNeighbours(grid, x, y);
+    }
+    void addToNeighbours (int[][] neighbourGrid, int x, int y) {
         for (int dy = -1; dy <= 1; dy++){
             for (int dx = -1; dx <= 1; dx++){
                 if (dx == 0 && dy == 0) continue; // Dont count center
-                n += (grid[(x + dx + size) % size][(y + dy + size) % size] & 0b1);
+                neighbourGrid[(x + dx + size) % size][(y + dy + size) % size] += 0b1 << 1;  // Increment from 2nd bit (neighbours begins there)
             }
         }
-        return n;
     }
 
     // Stores a snapshot every 10, 100, 1,000 and 10,000 steps
@@ -71,18 +141,19 @@ public class GameOfLife {
         boolean[][] aliveGrid = generateAliveGrid();
         for (int i = SNAPSHOTS; i > 0; i--) {
             int deltaStep = (int)Math.pow(10, i);
-            // Check
-            if (Arrays.deepEquals(aliveGrid, snapshots[i-1])) {     // Compare alive grid to snapshot
+
+            // Compare alive grid to snapshot
+            if (Arrays.deepEquals(aliveGrid, snapshots[i-1])) {
                 int steps = step % deltaStep;
                 if (steps == 0) steps += deltaStep;
                 
                 if (lowestRepeat > steps){
                     lowestRepeat = steps;
-                    System.out.println("Repeats itself every " + (lowestRepeat)+ " steps at:" + step);
+                    System.out.println("Repeats itself every " + (lowestRepeat)+ " steps at: " + step);
                 }
             }
 
-            // Add after checking
+            // Set snapshot after checking
             if (step % deltaStep == 0) {
                 snapshots[i-1] = aliveGrid;
             }
--- src/GameOfLifeMain.java
+++ src/GameOfLifeMain.java
@@ -2,15 +2,17 @@ import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class GameOfLifeMain {
-    static GameOfLife game;                 // ( ͡° ͜ʖ ͡°)
+    static GameOfLife game;                  // ( ͡° ͜ʖ ͡°)
 
-    static final boolean MANUAL = true;    // Sry sir, but i don't know how to drive stick!
-    static final int FPS = 60;              // ThE huMaN eyE caN't SeE moRe tHaN 24 fpS
+    static final boolean TIME_STAMPS = false;// Time stamps for every step
+    static final boolean DRAW_SIMPLE = true; // true = black and white, false = color (Simple = less resources)
+
+    static final boolean MANUAL = false;     // Sry sir, but i don't know how to drive stick!
+    static final int MAX_FPS = 120;          // ThE huMaN eyE caN't SeE moRe tHaN 24 fpS
 
     public static void main(String[] args) throws FileNotFoundException {
         int[][] map = loadMap("src/map/acorn.gol");
@@ -22,16 +24,22 @@ public class GameOfLifeMain {
         if (MANUAL)
             manualGame();
         else
-            automaticGame(FPS);
-
+            automaticGame(MAX_FPS);
     }
 
     // ==================== STARTING GAME  ===================== //
     static void automaticGame (int updatesPerSecond) {
-        int msDelay = 1000 / updatesPerSecond;
+        int msDelay = updatesPerSecond == 0 ? 0 : 1000 / updatesPerSecond;
         while(true) {
+            long start_time;
+            if (TIME_STAMPS)  start_time = System.nanoTime();
+
             game.nextState();
-            drawGOL(game, msDelay);
+
+            if (TIME_STAMPS) System.out.println("This operation took " + ((System.nanoTime() - start_time) / 1e6) + " ms");
+
+            if (DRAW_SIMPLE) drawGOLSimple(game, msDelay);
+            else drawGOL(game, msDelay);
         }
     }
     static void manualGame () {
@@ -39,9 +47,17 @@ public class GameOfLifeMain {
         Scanner console = new Scanner(System.in);
         while (running) {
             int n = getInput(console);
+
+            // TIME CALC
+            long start_time = System.nanoTime();
+
             for (int i = 0; i < n; i++)
                 game.nextState();
-            drawGOL(game, 1);
+
+            System.out.println("This operation took " + ((System.nanoTime() - start_time) / 1e9) + " seconds");
+
+            if (DRAW_SIMPLE) drawGOLSimple(game, 0);
+            else drawGOL(game, 0);
         }
     }
 
@@ -99,15 +115,29 @@ public class GameOfLifeMain {
         int size = gol.getSize();
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
-                boolean alive = (grid[x][y] & 0b1) != 0;
-                StdDraw.setPenColor(colorFancy(alive, grid[x][y] >> 1));
+                boolean alive = (grid[x][y] & 0b1) == 1;
+                StdDraw.setPenColor(colorFancy(alive, grid[x][y] >> 5));
                 StdDraw.filledSquare(x+.5f,y+.5f, 1/2f);
             }
         }
         StdDraw.show(msDelay);
     }
 
-    static final int CANVAS_SIZE = 1024;
+    // Draw the gameoflife in a simple texture (high performance)
+    static void drawGOLSimple(GameOfLife gol, int msDelay) {
+        StdDraw.clear();
+        int[][] grid = gol.getGrid();
+        int size = gol.getSize();
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                if ((grid[x][y] & 0b1) == 1)
+                    StdDraw.filledSquare(x+.5f,y+.5f, 1/2f);
+            }
+        }
+        StdDraw.show(msDelay);
+    }
+
+    static final int CANVAS_SIZE = 512;
     static void openWindow(GameOfLife gol){
         int size = gol.getSize();
 
@@ -115,6 +145,8 @@ public class GameOfLifeMain {
         StdDraw.setScale(0, size);
         StdDraw.show(1);
         drawGOL(gol, 1);
+        StdDraw.setPenColor(Color.black);
+
     }
 
 
